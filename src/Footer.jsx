fe81c20b0633e76359f10e27f573105a476db018
Angel Madin <madinangel05@gmail.com>
2023-04-03 00:00:22 +0300

    created home page

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import './styles/reset.css'
+import './styles/Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <p>ClimaFix 2023 &copy; All Rights Reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import './styles/reset.css'
+import './styles/Header.css'
+
+function Header() {
+    return (
+        <header className="header">
+            <h1 className="header-heading">ClimaFix</h1>
+            <img className="header-img" src="./src/images/logo.png" alt="logo"/>
+        </header>
+    )
+}
+
+export default Header
