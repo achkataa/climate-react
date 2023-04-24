c61b1bf3dd85cf66bf01b0229964c51242f56325
Petar Hamamdzhiev <petar@condor-gaming.com>
2023-04-24 18:48:41 +0300

    refactoring, using env variables

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mupjnkbasyrzlhwnigbj.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11cGpua2Jhc3lyemxod25pZ2JqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE2NTg3MjMsImV4cCI6MTk5NzIzNDcyM30.SIvULsQdhV93n21uti8INAY7RDsPXPJ8IYJ0S8QanRI'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 
